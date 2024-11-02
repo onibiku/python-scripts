c624b92a5147caf03bd91a1e21d0e59c4fa194be
onibiku <lin.victor.h@gmail.com>
2024-11-01 23:04:55 -0400

    adding character art

---
+++ funky_code.py
@@ -0,0 +1,30 @@
+
+char = "*"
+
+print(f" {char*4}")
+print(f"{char} ")
+print(f"{char} ")
+print(f"{char} ")
+print(f" {char*4}")
+print("")
+
+print(f" {char*4}")
+print(f"{char}   {char}")
+print(f"{char}   {char}")
+print(f"{char}   {char}")
+print(f" {char*4}")
+print("")
+
+print(f"{char*3}")
+print(f"{char}  {char}")
+print(f"{char}   {char}")
+print(f"{char}  {char}")
+print(f"{char*3}")
+print("")
+
+print(f"{char*5}")
+print(f"{char}")
+print(f" {char*4}")
+print(f"{char}")
+print(f"{char*5}")
+print("")
